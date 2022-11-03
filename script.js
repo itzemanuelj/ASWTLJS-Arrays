7f15cfb2c5038ca64405c4d0c15ff5e75685f941
Emanuel Joness <itzemanuelj@gmail.com>
2022-11-03 00:00:34 -0400

    inital commit for arrays in js

---
+++ script.js
@@ -0,0 +1,17 @@
+// What is a variable that represents a list of values? Answer with one word.
+//array
+
+// An array comprises any number of ______. ("Values" would be correct, but I'm looking for another word.)
+//elements
+
+// The number inside the square brackets is the element's ____.
+// 
+
+// In an array, the numbering starts with __.
+0
+
+// Create a new array of strings.
+let numsAsStrings = ['1','2','3','4','4','6'];
+
+// Create a new array, nums, comprising three numbers from 1 to 5.
+let nums = [1,2,3,4,5];
