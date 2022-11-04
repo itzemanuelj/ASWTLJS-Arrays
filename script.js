a693a99d61f02ceba243351f9cca451162fbf9b3
Emanuel Joness <itzemanuelj@gmail.com>
2022-11-04 19:00:34 -0400

    mvp commmit for arrays in js

--- script.js
+++ script.js
@@ -13,5 +13,72 @@
 // Create a new array of strings.
 let numsAsStrings = ['1','2','3','4','4','6'];
 
-// Create a new array, nums, comprising three numbers from 1 to 5.
+// Create a new array, nums, comprising three numbers from 11 to 5.
 let nums = [1,2,3,4,5];
+
+// This array has been defined.
+
+var names = ["Alf", "Bo", "Gail"];
+
+// Specifying an array element, code an alert that displays "Gail".
+alert(names[2]);
+
+// Create an array with a single element, a string. The array hasn't been declared beforehand.
+
+let singleElement = ['Justin'];
+
+// Create an array with three elements that are numbers. It hasn't been declared beforehand.
+
+let number = [1,2,3];
+
+// Assign to a variable the first element of an array. The variable hasn't been declared beforehand.
+
+let arrayVariables = cost[0];
+
+// Create an array with three elements: a number, a string, and a variable. The array has been declared beforehand.
+
+let threeArray = [1,'hello world',time];
+
+// Create an array with two string elements. Then assign the last element in the array to a variable. Neither the array nor the variable have been declared beforehand.
+
+stringArray = ['justin','jones'];
+lName = singleElement[1];
+                            
+// This array has been defined.
+var names = ["Alf", "Bo", "Gail"];
+// Specifying an array element, code an alert that displays "Gail".
+
+alert(names[2]);
+
+// Create an array with one string element. The array hasn't been declared beforehand.
+oneStringeElement = ['hello world'];
+
+// Create an array with three elements that are numbers. It hasn't been declared beforehand.
+
+arrayNumber = [1,2,3,4,5,6];
+
+// Assign to a variable the first element of an array. The variable hasn't been declared beforehand.
+cost = time[0];
+
+// Create an array with three elements: a number, a string, and a variable. The array has been declared beforehand.
+
+arrayWithThreeElements = [1,'one',one];
+
+// Create an array with two string elements. Then assign the last element in the array to a variable. Neither the array nor the variable have been declared beforehand.
+
+var lNames = ['jones','emanuel'];
+myLastName = lNames[1];
+
+// Declare an array with three elements.
+// Display the last element in an alert.
+
+arrayWith3Elements = ['hello','world','!'];
+alert(arrayWith3Elements[2]);
+
+// Live coding exercise:
+
+// Declare an array with four elements.
+// Display the first element in an alert.
+
+arrayWith4Elements = ['hello','world','!','!'];
+alert(arrayWith3Elements[0]);
